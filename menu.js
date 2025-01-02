fdcc30be31653d4db88130ece395c3b4917e3d4c
Md Kazi Musfiqur Rahman <musfiqur2205101505@diu.edu.bd>
2025-01-03 00:47:53 +0600

    1st Commit

---
+++ menu.js
@@ -0,0 +1,55 @@
+document.getElementById('chatDot').addEventListener('click', function () {
+    const chatMenu = document.getElementById('chat-menu');
+    chatMenu.style.display = chatMenu.style.display === 'none' ? 'block' : 'none';
+
+});
+
+
+		document.getElementById('chatDot3').addEventListener('click', function () {
+    const chatMenu = document.getElementById('chat-menu3');
+    chatMenu.style.display = chatMenu.style.display === 'none' ? 'block' : 'none';
+
+});
+
+
+		document.getElementById('chatDot2').addEventListener('click', function () {
+    const chatMenu = document.getElementById('chat-menu2');
+    chatMenu.style.display = chatMenu.style.display === 'none' ? 'block' : 'none';
+
+});
+
+
+		document.getElementById('chatDot4').addEventListener('click', function () {
+    const chatMenu = document.getElementById('chat-menu4');
+    chatMenu.style.display = chatMenu.style.display === 'none' ? 'block' : 'none';
+
+});
+
+
+		document.getElementById('chatDot5').addEventListener('click', function () {
+    const chatMenu = document.getElementById('chat-menu5');
+    chatMenu.style.display = chatMenu.style.display === 'none' ? 'block' : 'none';
+
+});
+
+
+		document.getElementById('chatDot6').addEventListener('click', function () {
+    const chatMenu = document.getElementById('chat-menu6');
+    chatMenu.style.display = chatMenu.style.display === 'none' ? 'block' : 'none';
+
+});
+
+
+		document.getElementById('chatDot7').addEventListener('click', function () {
+    const chatMenu = document.getElementById('chat-menu7');
+    chatMenu.style.display = chatMenu.style.display === 'none' ? 'block' : 'none';
+
+});
+
+
+		document.getElementById('chatDot8').addEventListener('click', function () {
+    const chatMenu = document.getElementById('chat-menu8');
+    chatMenu.style.display = chatMenu.style.display === 'none' ? 'block' : 'none';
+
+});
+
